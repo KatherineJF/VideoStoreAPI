6d14e0da5098a488e7d3aab03a012ce9c8ce3a65
KatherineJF <kjfitzpat@hotmail.com>
2018-11-06 14:01:41 -0800

    movie inventory increase and decrease methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,19 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
   validates :title, presence: true
+
+  def can_checkout?
+    self.inventory >= 1
+  end
+
+  def increment_inventory
+    self.inventory += 1
+    self.save
+  end
+
+  def decrement_inventory
+    self.inventory -= 1
+    self.save
+  end
+
 end
