e84c17c3c4460e2ad3af2fbb836f9c081621270e
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-11-06 13:27:23 -0800

    show tests passed for movie controller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -38,37 +38,28 @@ describe MoviesController do
     end
 end
 
-describe "show" do
-  #
-  #   it "should get show" do
-  #     get movie_path(1)
-  #     value(response).must_be :success?
-  #   end
-  #
-  #
-  #   # This bit is up to you!
-  #   it "can get a movie" do
-  #     get movie_path(movies(movie2).id)
-  #     must_respond_with :success
-  #   end
-  #
-  #   it "responds with a 404 message if no movie is found" do
-  #     id = -1
-  #     get movie_path(id)
-  #     must_respond_with :not_found
-  #   end
-  #
-  #
-  #
-  # it "should get create" do
-  #   get movies_create_url
-  #   value(response).must_be :success?
-  # end
-  #
-  # it "should get update" do
-  #   get movies_update_url
-  #   value(response).must_be :success?
-  # end
-end
+  describe "show" do
+
+    it "is a real working route and returns JSON" do
+      id = movies(:movie1).id
+
+      get movie_path(id)
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+
+    it "can get a movie" do
+      get movie_path(movies(:movie2).id)
+      must_respond_with :success
+    end
+
+    it "responds with a 404 message if given invalid id" do
+      id = -1
+      get movie_path(id)
+      must_respond_with :not_found
+    end
+
+  end
 
 end
