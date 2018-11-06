9a5fdebe6a3797b0fe511897f053cb13db3113f4
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-11-06 13:25:11 -0800

    index controller tests passing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
 
   def index
       movies = Movie.all
-      render json: movies, status: :ok
+      render json: movies.as_json(except: [:created_at, :updated_at]), status: :ok
   end
 
     def show
